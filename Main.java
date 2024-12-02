46a2973a20108b30d743c561d5fb70c76bf90a68
SudeekshaVardhan <svardhan0413@gmail.com>
2024-12-02 15:18:48 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.*;
 import javax.swing.*;
 
+
 public class Main
 {
     public static void main(String[] args)
@@ -13,7 +14,14 @@ public class Main
         //Creates a new board with a minimum size of (1000, 1000) (x, y)
         frame.setMinimumSize(new Dimension(1000,1000));
 
+        frame.setLayout(new GridBagLayout());
 
+        //Centers the board in the middle of the screen
+        frame.setLocationRelativeTo(null);
+
+        Board board = new Board();
+        //frame.add(board);
+        
         frame.setVisible(true);
 
 
