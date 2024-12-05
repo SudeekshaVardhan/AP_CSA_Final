ed864a1b6defd780eacd947e2bd9cb5e7db9b5af
SudeekshaVardhan <svardhan0413@gmail.com>
2024-12-05 14:11:31 -0800

    New info for each to get images

--- Chess_Game/piece/Knight.java
+++ Chess_Game/piece/Knight.java
@@ -1,5 +1,22 @@
 package Chess_Game.piece;
 
-public class Knight {
+import Chess_Game.main.GamePanel;
+
+public class Knight extends Piece{
+
+    public Knight(int color, int col, int row)
+    {
+        super(color, col, row);
+
+        if(color == GamePanel.WHITE)
+        {
+            image = getImage("/piece/knight");
+        }
+
+        else
+        {
+            image = getImage("/piece/knight1");
+        }
+    }
     
 }
--- Chess_Game/piece/Rook.java
+++ Chess_Game/piece/Rook.java
@@ -7,6 +7,16 @@ public class Rook extends Piece{
     public Rook(int color, int col, int row)
     {
         super(color, col, row);
+
+        if(color = GamePanel.WHITE)
+        {
+            image = getImage("/piece/rook");
+        }
+
+        else
+        {
+            image = getImage("/piece/rook1");
+        }
     }
 
 }
